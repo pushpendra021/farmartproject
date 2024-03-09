de527aa6c80d6756e35275e049c4524534f25b26
pushpendra021 <pushpendrabais021@gmail.com>
2024-03-09 11:30:47 +0530

    Today install rect-router-dom and setup, create browserRoute,routes and route ,and also create multiple js files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './frontend/navbar';
 // import Footer from './frontend/footer';
 // import Slider from './frontend/slider';
 // import { slidesData } from "./frontend/slidesData";
+import Home from "./frontend/Home";
+import About from "./frontend/About";
+import Product from "./frontend/Product";
+import Contact from "./frontend/Contact";
+import SingleProduct from "./frontend/SingleProduct";
+import Cart from "./frontend/Cart";
+import ErrorPage from "./frontend/ErrorPage";
 function App() {
   return (
-   <div>
-<Navbar></Navbar>
-<br></br>
-<br></br>
 
-  {/* <Slider slides={slidesData} /> */}
+<BrowserRouter>
 
-<br></br>
+<Routes>
+  <Route path="/" element={<Home></Home>}></Route>
+  <Route path="/about" element={<About></About>}></Route>
+  <Route path="/product" element={<Product></Product>}></Route>
+  <Route path="/contact" element={<Contact></Contact>}></Route>
+  {/* show the single product with different images */}
+  <Route path="/singleproduct/;id" element={<SingleProduct></SingleProduct>}></Route> 
+  <Route path="/cart" element={<Cart></Cart>}></Route> 
+  {/* error page if another than the above page than show the error page */}
+  <Route path="*" element={<ErrorPage></ErrorPage>}></Route> 
+</Routes>
 
-{/* <Footer></Footer> */}
+</BrowserRouter>
 
-   </div>
+//    <div>
+// <Navbar></Navbar>
+// <br></br>
+// <br></br>
+
+//   {/* <Slider slides={slidesData} /> */}
+
+// <br></br>
+
+// {/* <Footer></Footer> */}
+
+//    </div>
   );
 }
 
