7b060a72d235fafa1037915dc9be26f54a6e183d
ramansingh6258 <ramansingh6150032@gmail.com>
2024-03-13 16:47:00 +0530

    created services and trusted section in home

--- src/frontend/Home.js
+++ src/frontend/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
 import HeroSection from "./HeroSection";
+import Services from "./services";
+import Trusted from "./trusted";
 
 const Home = () => {
   const data = {
     name: "FarMart",
   };
 
-  return <HeroSection myData={data} />;
+  return (
+    <>
+      <HeroSection myData={data} />
+      <Services />
+      <Trusted />
+    </>
+  );
 };
 
 export default Home;
